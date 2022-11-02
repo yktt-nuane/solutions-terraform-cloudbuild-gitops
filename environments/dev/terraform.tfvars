9d9a18c50886cd6ca4161336e382edff4f0a5853
yktt-nuane <tyinnagoyauniversity@gmail.com>
2022-11-02 02:59:09 +0000

    Update project IDs and buckets

--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="etude-terraform-gcp"
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "etude-terraform-gcp-tfstate"
     prefix = "env/prod"
   }
 }
